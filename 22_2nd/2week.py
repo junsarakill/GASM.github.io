95c38c2ad1aeb1051f26ab2b924de1f132ced089
junsarakill <bs9620@gmail.com>
2022-09-14 20:29:22 +0900

    Update 2week.py

--- 22_2nd/2week.py
+++ 22_2nd/2week.py
@@ -1,1 +1,18 @@
-print("hello, world!")
+doremi = {
+    1 : "a"
+    ,2 : "b"
+    ,3 : "c"
+    ,4 : "d"
+}
+
+aa = "gerrilla"
+
+print(aa[2:])
+
+print(doremi.get(3))
+
+print("""
+ojodajsdadad
+ad
+ffvvoxckpdkqowdaskz;dzca;ldkpo2kq24e3k43r
+""")
