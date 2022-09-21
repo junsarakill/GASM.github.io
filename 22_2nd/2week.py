980ce652a3a9361eba764a4b50b96ef3b60d1565
junsarakill <61420467+junsarakill@users.noreply.github.com>
2022-09-21 20:31:58 +0900

    Update 2week.py

--- 22_2nd/2week.py
+++ 22_2nd/2week.py
@@ -1,18 +1,13 @@
-doremi = {
-    1 : "a"
-    ,2 : "b"
-    ,3 : "c"
-    ,4 : "d"
-}
+#userInput = input("input num1: ")
+#userInput = float(userInput) %2
 
-aa = "gerrilla"
-
-print(aa[2:])
-
-print(doremi.get(3))
-
-print("""
-ojodajsdadad
-ad
-ffvvoxckpdkqowdaskz;dzca;ldkpo2kq24e3k43r
-""")
+#print(userInput)
+print(int(1.2 + (2.3-4.5) + 2.5))
+print(type(1.0), type(1), type(1/2))
+print(2**10)
+print(2**1.25)
+print(8.5//2)
+print(2e10, 2e-2)
+print(0x10,0b10,0o10)
+print(hex(10))
+print(123 < 111 or 23323 < 23323232323232)
