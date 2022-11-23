2c9b0dd92e7c3cc4d4241ee1dd5d06ff4749db44
junsarakill <61420467+junsarakill@users.noreply.github.com>
2022-11-23 19:16:57 +0900

    study

--- 22_2nd/10week.py
+++ 22_2nd/10week.py
@@ -10,8 +10,22 @@ class Demi():
         self.c = c
     def move (self, speed):
         print("자전거 : {}km/h 로 전진".format(speed))
+    def turn (self, direction):
+        print("자전거: {} 회전".format(direction))
     def stop (self):
         print("bike({},{}) : stop".format(self.ws,self.c))
+    
+class pedoDemi(Demi):
+    def __init__(self,ws,c,state):
+        Demi.__init__(self,ws,c)
+        self.state = state
+    
+    def fold(self):
+        self.state = "folding"
+        print("bike: fold, state = {0}".format(self.state))
+    def unfold(self):
+        self.state = "unfolding"
+        print("bike: unfold, state= {}".format(self.state))
 
 demi = Demi(25,"blue")
 print(demi.asd)
@@ -20,4 +34,10 @@ demi.named = "jkakk"
 print(demi.named)
 
 demi.move(30)
+demi.turn("right")
 demi.stop()
+
+pedo = pedoDemi(11, "white","folding")
+pedo.move(30)
+pedo.fold()
+pedo.stop()
