fd6fc3ed4360995a637113fec33360211320c788
junsarakill <61420467+junsarakill@users.noreply.github.com>
2023-10-20 18:32:05 +0900

    1020
    2차원 배열 visualize
    rt는 anchoredPosition으로 위치 지정가능

--- MASS/MetaSelfStudy/Assets/01_Scripts/GridUIManager.cs
+++ MASS/MetaSelfStudy/Assets/01_Scripts/GridUIManager.cs
@@ -10,6 +10,8 @@ public class GridUIManager : MonoBehaviour
     //가로 세로 개수
     [Range(1,10)][SerializeField] int width,height;
 
+    [SerializeField] float padding;
+
     //그리드
     [SerializeField]Transform gridTR;
     //시작 버튼
@@ -49,19 +51,60 @@ public class GridUIManager : MonoBehaviour
             Destroy(child.gameObject);
         }
         
+        //원소 인덱스
         int index = 0;
 
-        for(int i = 0; i < height; i ++)
+        //RectTransform rt ;
+        //rt.anchoredPosition = new Vector2(100, 100);
+
+
+         
+        //방법 1 이중 for
+
+        //for(int i = 0; i < height; i ++)
+        //{
+        //    //열 생성
+        //    GameObject column = Instantiate(columnF, gridTR);
+        //    //위치 이동
+        //    column.GetComponent<RectTransform>().Translate(0,i*(100 + padding),0);
+        //    for(int j = 0; j < width; j ++)
+        //    {
+        //        //원소 생성
+        //        GameObject element = Instantiate(elementF, column.transform);
+        //        //위치 이동
+        //        element.GetComponent<RectTransform>().Translate(j*(100 + padding),0,0);
+        //        element.GetComponent<Element>().text.text = $"{index}";
+        //        //인덱스 및 행 위치 증가
+        //        index++;
+        //    }
+        //}
+
+        //방법 2 while 이랑 dowhile
+        while(index < width*height)
         {
-            //열 생성
-            GameObject column = Instantiate(columnF, gridTR);
-            for(int j = 0; j < width; j ++)
+            //나머지가 0일때 열 생성
+            if(index % width == 0)
             {
-                //원소 생성
-                GameObject element = Instantiate(elementF, column.transform);
-                element.GetComponent<Element>().text.text = $"{index}";
-                index++;
+                GameObject column = Instantiate(columnF, gridTR);
+                //위치 이동
+                column.GetComponent<RectTransform>().Translate(0,(index/width)*(100 + padding),0);
+
+                //원소 하나 생성하고 다시 나머지가 0이 될때까지 원소 생성
+                do
+                {
+                    GameObject element = Instantiate(elementF, column.transform);
+                    //위치 이동
+                    element.GetComponent<RectTransform>().Translate((index % width)*(100 + padding),0,0);
+                    element.GetComponent<Element>().text.text = $"{index}";
+                    index++;
+                }
+                while(index % width != 0);
             }
         }
+    }
+
+    void SetTRPosition()
+    {
+
     }
 }
