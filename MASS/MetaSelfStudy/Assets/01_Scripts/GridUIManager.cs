88dced4df11e0b333d0a94346be10117893a50fe
junsarakill <61420467+junsarakill@users.noreply.github.com>
2023-10-20 09:29:23 +0900

    1020 array visualize
    완성했지만 layout group 컴포넌트 없이 코드만으로 정렬 시키기

--- MASS/MetaSelfStudy/Assets/01_Scripts/GridUIManager.cs
+++ MASS/MetaSelfStudy/Assets/01_Scripts/GridUIManager.cs
@@ -9,10 +9,59 @@ public class GridUIManager : MonoBehaviour
 {
     //가로 세로 개수
     [Range(1,10)][SerializeField] int width,height;
-    
+
+    //그리드
+    [SerializeField]Transform gridTR;
+    //시작 버튼
+    [SerializeField]Button startBtn;
+
+    //원소 프리팹
+    [SerializeField]GameObject elementF;
+    //column 프리팹
+    [SerializeField]GameObject columnF;
+
+
+    /*
+    w = 3
+    h = 2
+
+    column 1 | e1,e2,e3
+    column 2 | e1,e2,e3
+    */
+
     private void Start() {
-        string a = "";
-        Enumerable.Range(1,10).ToList().ForEach( (int i) => a += i);
+        startBtn.onClick.AddListener(MakeGraph);
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            MakeGraph();
+        }
+    }
+
+    //가로 세로 받아서 표 만들기
+    void MakeGraph()
+    {
+        //표 비우기
+        foreach(Transform child in gridTR)
+        {
+            Destroy(child.gameObject);
+        }
         
+        int index = 0;
+
+        for(int i = 0; i < height; i ++)
+        {
+            //열 생성
+            GameObject column = Instantiate(columnF, gridTR);
+            for(int j = 0; j < width; j ++)
+            {
+                //원소 생성
+                GameObject element = Instantiate(elementF, column.transform);
+                element.GetComponent<Element>().text.text = $"{index}";
+                index++;
+            }
+        }
     }
 }
