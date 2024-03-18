bc9dd7af1d1f3b20d2b7b34160016d3389d7f3a0
junsarakill <61420467+junsarakill@users.noreply.github.com>
2024-03-18 17:52:03 +0900

    240318 dfs 작성중

--- SKGS/V3/SKGS.js
+++ SKGS/V3/SKGS.js
@@ -37,14 +37,15 @@ class imgInfo
        [5] : 속성 - 0:blue 1:red 2:yellow 3:purple 4:green
        [6:7] : 출시 순서 - 00~99 속성이 같을 시 출시 순에 따라 00,01,02 순으로 지어짐
      */
-    constructor(img_name = "cmn_cx87654321")
+    constructor(img_name = "cmn_cx87654321.png")
     {
+        this.file_name = img_name;
         // 전문 6번째부터 끝까지가 id
-        this.id = img_name.substring(6);
+        this.id = img_name.substring(6,14);
         // id 0 번째 레어도
         this.rarity = this.id[0];
         // 이름 번호
-        this.name = this.id.substring(2,4);
+        this.name = this.id.substring(2,5);
         // 타입
         this.type = this.id[5];
         // 활성여부
@@ -65,6 +66,61 @@ class imgInfo
     }
 }
 
+// 사실상 main
+// 이미지 객체 생성
+async function aCreateImgInfo()
+{
+    // 이미지 명 가져오기
+    const img_names = await getImgNames();
+
+    console.log(img_names);
+
+    // 객체 생성
+    let img_infos = createImgInfo(img_names);
+
+    // 이름과 속성 순 정렬
+    img_infos = sortImgInfo(img_infos);
+
+    // 관계도 맵 생성
+    const rel_map = buildImgRelation(img_infos);
+
+    console.log(rel_map);
+
+    // 이미지 그루핑
+    const img_groups = imgGrouping(img_infos, rel_map);
+
+    console.log(img_groups);
+
+}
+
+// 인접 리스트 생성
+function createAdjList(rel_map)
+{
+    const adj_list = {};
+    rel_map.forEach(map => {
+        const [from, to] = Object.entries(map)[0];
+
+        if(!adj_list[from])
+            adj_list[from] = [];
+
+        adj_list[from].push(to);
+    });
+
+    return adj_list;
+}
+
+// 이미지 그루핑용 dfs
+// @@ 작성중 뤼튼 참조
+function dfs(node, adj_list, )
+{
+    
+}
+
+
+// 이미지 그루핑
+
+
+
 // 이미지 간 관계 설정
 // 기본은 [1] 말고 같은 숫자일때 관계가 있는데, 예외도 있음(4로 시작할때나 속성이 바뀌는 경우)
 function buildImgRelation(img_infos)
@@ -82,33 +138,38 @@ function buildImgRelation(img_infos)
             const nni = next_id.substring(0, 1) + next_id.substring(2);
 
             if (cci === nni) 
+            {
                 // 관계가 확인되면 Map에 추가합니다.
                 RELATION_MAP.set(cur_id, next_id);
+            }
         }
     });
 
     return RELATION_MAP;
 }
 
-// 이미지 객체 생성
-async function aCreateImgInfo()
+// 객체 생성
+function createImgInfo(img_names)
 {
-    // 이미지 명 가져오기
-    const img_names = await getImgNames();
-
-    console.log(img_names);
-
-    // 객체 생성
-    let img_infos = [];
+    img_infos = [];
     img_names.forEach(img_name => {
         img_infos.push(new imgInfo(img_name));
     });
 
-    console.log(img_infos);
+    return img_infos;
+}
 
-    const rel_map = buildImgRelation(img_infos);
+// 이미지 정보 객체 이름과 속성 순 정렬
+function sortImgInfo(img_infos)
+{
+    img_infos.sort((a,b) => {
+        if(a.name != b.name)
+            return a.name.localeCompare(b.name);
+        else
+            return a.type.localeCompare(b.type);
+    });
 
-    console.log(rel_map);
+    return img_infos;
 }
 
 // 이미지명 가져오기
@@ -124,11 +185,12 @@ function getImgNames()
             let img_names = [];
 
             data.forEach(file => {
-                // 파일을 읽어서 이미지로 출력
-                const imgElement = document.createElement("img");
-                imgElement.src = file.download_url;
-                document.getElementById("list_content").appendChild(imgElement);
-
+                // // 파일을 읽어서 이미지로 출력
+                // const imgElement = document.createElement("img");
+                // imgElement.src = file.download_url;
+                // document.getElementById("list_content").appendChild(imgElement);
+                
+                // @@ 나중에는 조건 달아서 필요한 이미지만 가져와야 할지도
                 // 이미지명 가져오기
                 img_names.push(file.name);
             });
