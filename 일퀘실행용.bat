b6ac21a778449c8d368dc4e5a8ecd8ef43371277
junsarakill <bs9620@gmail.com>
2024-08-03 20:58:27 +0900

    Create 일퀘실행용.bat

---
+++ 일퀘실행용.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:: cd "C:\Users\wjs96\AppData\Roaming\Microsoft\Windows\Start Menu"
+:: start LDPlayer9
+
+cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\jk_games"
+start GF_UE
+
+cd C:\Users\wjs96\Downloads\LongSaveFiles\gf_alarm\GF_Alarm
+start GFAlarm
+
+rem cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\ONE store"
+rem start ONEGameLoop
+
+cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\jk_games"
+start SKNL
+
+start dmmgameplayer://play/GCL/dolwav/cl/win
+
+:: cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
+:: start DMMGamePlayer
